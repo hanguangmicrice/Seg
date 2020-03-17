cd8d3b74129f127471657b342250c674cfa10e02
Han <William.Han@ap.jll.com>
2020-03-17 10:21:22 +0800

    fusion part complete

--- gscnn_fusion_block.py
+++ gscnn_fusion_block.py
@@ -37,15 +37,20 @@ class ASPP(nn.Module):
         super(ASPP, self).__init__()
         self._aspp = nn.Module()
         self._aspp.add_module('conv1',conv1x1(in_channel,out_channel,1,0,1))
-        self._aspp.add_module('imgpool',ImagePooling(in_channel,out_channel))
         for key, value in enumerate(rates):
             self._aspp.add_module("c{0}".format(key+1),
                                     conv3x3(in_channel,out_channel,stride=1,padding=value, dilation=value))
-        self._aspp.add_module('dedge_conv',conv1x1(1,out_channel))
-        self.conv1 = conv1x1(out_channel*5, out_channel, stride=1,padding=0,dilation=1)
-    def forward(self,x):
-        result = torch.cat([stage(x) for stage in self._aspp.children()], dim=1)
+        self._aspp.add_module('edge_conv',conv1x1(1,out_channel))
+        self._aspp.add_module('imgpool',ImagePooling(in_channel,out_channel))
+        self.conv1 = conv1x1(out_channel*6, out_channel, stride=1,padding=0,dilation=1)
+    def forward(self,x,edge):
+        _,_,h,w = x.shape
+        img_pool_features = self._aspp.imgpool(x)
+        edge_featues = F.interpolate(edge,(h,w),mode='bilinar',align_corners=True)
+        edge_featues = self._aspp.edge_conv(edge_featues)
+        img_edge_features = torch.cat((edge_featues,img_pool_features), dim=1)
+
+        result = torch.cat([stage(x) for stage in self._aspp.children()[:-2]], dim=1)
+        result = torch.cat((img_edge_features,result), dim=1)
         result = self.conv1(result)
         return result
-
-        
